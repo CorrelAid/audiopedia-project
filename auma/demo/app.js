497fafadb35db7caa7b6141997d55a9def3cd48f
Peter Byfield <byfield554@gmail.com>
2021-08-24 22:29:02 +0200

    Add demo image

--- auma/demo/app.js
+++ auma/demo/app.js
@@ -4,42 +4,52 @@ auma({
     {
       id: "slept-badly",
       audioUrl: "/mp3/slept-badly.mp3",
+      imageUrl: "/img/brain.jpg",
     },
     {
       id: "crying-more",
       audioUrl: "/mp3/crying-more.mp3",
+      imageUrl: "/img/brain.jpg",
     },
     {
       id: "not-enjoying-daily-activities",
       audioUrl: "/mp3/not-enjoying-daily-activities.mp3",
+      imageUrl: "/img/brain.jpg",
     },
     {
       id: "hard-to-make-decisions",
       audioUrl: "/mp3/hard-to-make-decisions.mp3",
+      imageUrl: "/img/brain.jpg",
     },
     {
       id: "daily-life-suffering",
       audioUrl: "/mp3/daily-life-suffering.mp3",
+      imageUrl: "/img/brain.jpg",
     },
     {
       id: "playing-a-useful-part",
       audioUrl: "/mp3/playing-a-useful-part.mp3",
+      imageUrl: "/img/brain.jpg",
     },
     {
       id: "ending-life",
       audioUrl: "/mp3/ending-life.mp3",
+      imageUrl: "/img/brain.jpg",
     },
     {
       id: "feeling-tired",
       audioUrl: "/mp3/feeling-tired.mp3",
+      imageUrl: "/img/brain.jpg",
     },
     {
       id: "having-headaches",
       audioUrl: "/mp3/having-headaches.mp3",
+      imageUrl: "/img/brain.jpg",
     },
     {
       id: "poor-digestion",
       audioUrl: "/mp3/poor-digestion.mp3",
+      imageUrl: "/img/brain.jpg",
     },
   ],
 });
