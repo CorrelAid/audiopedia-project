db95f6993ccb2bb50649d1d06a745a7dc7c8f839
Peter Byfield <byfield554@gmail.com>
2021-09-04 17:50:23 +0200

    Declare audio component icons as local components

--- auma/src/audio.js
+++ auma/src/audio.js
@@ -1,6 +1,36 @@
 import "./audio.css";
 
+const IconPlay = {
+  template: `
+  <svg xmlns="http://www.w3.org/2000/svg" width="48" viewBox="0 0 48 48">
+    <circle cx="24" cy="24" r="24" fill="#ddd"/>
+    <path d="M3 22v-20l18 10-18 10z" fill="#000" transform="translate(14, 12)"/>
+  </svg>`,
+};
+
+const IconPause = {
+  template: `
+  <svg xmlns="http://www.w3.org/2000/svg" width="48" viewBox="0 0 48 48">
+    <circle cx="24" cy="24" r="24" fill="#ddd"/>
+    <path d="M11 22h-4v-20h4v20zm6-20h-4v20h4v-20z" fill="#000" transform="translate(12, 12)"/>
+  </svg>`,
+};
+
+const IconReplay = {
+  template: `
+  <svg xmlns="http://www.w3.org/2000/svg" width="48" viewBox="0 0 48 48">
+    <circle cx="24" cy="24" r="24" fill="#ddd"/>
+    <path d="M18.885 3.515c-4.617-4.618-12.056-4.676-16.756-.195l-2.129-2.258v7.938h7.484l-2.066-2.191c2.82-2.706 7.297-2.676 10.073.1 4.341 4.341 1.737 12.291-5.491 12.291v4.8c3.708 0 6.614-1.244 8.885-3.515 4.686-4.686 4.686-12.284 0-16.97z"
+      fill="#000" transform="translate(12, 12)"/>
+  </svg>`,
+};
+
 export const Audio = {
+  components: {
+    "icon-play": IconPlay,
+    "icon-pause": IconPause,
+    "icon-replay": IconReplay,
+  },
   template: `
 <div class="audio">
     <button v-if="state == 'start' || state == 'paused'" @click="play"><icon-play/></button>
