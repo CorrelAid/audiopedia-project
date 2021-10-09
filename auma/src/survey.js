a9e7b61f2243f97bb0457ef8d24b1e7089dddb53
Peter Byfield <byfield554@gmail.com>
2021-10-09 11:04:54 +0200

    Implement progress bar

--- auma/src/survey.js
+++ auma/src/survey.js
@@ -5,22 +5,37 @@ import "./survey.css";
 const IconChoiceYes = {
   template: `
 <svg xmlns="http://www.w3.org/2000/svg" width="64" viewBox="0 0 24 24">
-  <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/>
+  <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z" fill="#000"/>
 </svg>`,
 };
 
 const IconChoiceNo = {
   template: `
 <svg xmlns="http://www.w3.org/2000/svg" width="64" viewBox="0 0 24 24">
-  <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/>
+  <path 
+    d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"
+    fill="#000"/>
 </svg>`,
 };
 
 const IconChoiceSkip = {
   template: `
 <svg xmlns="http://www.w3.org/2000/svg" width="32" viewBox="0 0 24 24">
-  <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/>
+  <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" fill="#000"/>
 </svg>`,
+};
+
+const Progress = {
+  template: `
+<div>
+  <svg viewBox="0 0 100 5">
+    <rect width="100" height="5" fill="#ddd"/>
+    <rect :width="percentage" height="5" fill="#000" style="transition: width 1s ease;"/>
+  </svg>
+</div>`,
+  props: {
+    percentage: Number,
+  },
 };
 
 export const Survey = {
@@ -28,6 +43,7 @@ export const Survey = {
     "icon-choice-yes": IconChoiceYes,
     "icon-choice-no": IconChoiceNo,
     "icon-choice-skip": IconChoiceSkip,
+    "app-progress": Progress,
     "app-audio": Audio,
   },
   template: `
@@ -38,6 +54,10 @@ export const Survey = {
     @ready="onAudioReady"
     @ended="onAudioEnded" 
     @replay="onAudioReplay"/>
+
+  <app-progress 
+    :percentage="percentageProgress"
+    class="survey__progress"/>
 
   <div class="survey__options" :class="{disabled: optionsDisabled}">
     <button 
@@ -82,6 +102,9 @@ export const Survey = {
   computed: {
     currentQuestion() {
       return this.questions[this.currentQuestionIdx];
+    },
+    percentageProgress() {
+      return 100 * (this.currentQuestionIdx / this.questions.length);
     },
   },
   methods: {
